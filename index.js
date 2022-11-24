120622fb1b6120a91d6c8d204aa2bd5333258bfa
Rodgers Ogada <ogadarodgers@gmail.com>
2022-11-24 12:59:35 +0300

    Made changes to have mondayWork defined using function expression

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
+// defines saturdayFun function declaration as specified:
+// ✓ function exists
+// ✓ uses the default activity 'roller-skate' when no arguments are passed
+// ✓ permits the default activity to be overriden
+
 function saturdayFun(activity = "roller-skate") {
     return `This Saturday, I want to ${activity}!`;
 }
 
-function mondayWork(activity = "go to the office") {
-    return `This Monday, I will ${activity}.`
+// defines mondayWork function expression as specified
+// ✓ function exists
+// ✓ uses the default activity 'go to the office' when no arguments are passed
+// ✓ permits the default activity to be override
+
+const mondayWork = function (activity = "go to the office") {
+    return`This Monday, I will ${activity}.`;
 }
 
-// wrapAdjective("%")("a dedicated programmer"); 
-//=> "You are %a dedicated programmer%!"
-/*
-function wrapAdjective(initialText = "You are") {
-    const messageFlair = function (flairContent = "a dedicated programmer") {
-        return `${initialText} ${flairContent}!`;
-    }
-    return messageFlair;
-}
-*/
+// defines wrapAdjective function according to the specification
+// ✓ function exists
+// ✓ when initialized with '*' creates a function that, when called, wraps an adjective in a highlight
+// ✓ when initialized with '||' creates a function that, when called, wraps an adjective in a highlight
+
 function wrapAdjective (type = "*") {
     const flairing = function(adjective = "special") {
         return `You are ${type}${adjective}${type}!`;
